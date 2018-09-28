d3feb374f02b16af36a02599a478e6f989b10985
Cole <2bigshot@192.168.0.110>
2018-09-27 23:29:46 -0600

    First Addition

---
+++ src/Lists/QueueExample.java
@@ -0,0 +1,39 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Lists;
+
+import java.util.LinkedList;
+import java.util.Queue;
+
+/**
+ *
+ * @author Cole
+ */
+public class QueueExample {
+
+    public QueueExample() {
+        Queue<Integer> queue = new LinkedList<>();
+        //fills queue with 100 to 0   
+        System.out.println("Loads the queue with the following:");
+        for (int i = 25; i > 0; i--) {
+            queue.add(i);
+            System.out.print(i + " ");
+        }
+        
+        System.out.println("\n\nRemoves each memeber one by one");
+        
+        //outputs queue
+        System.out.println();
+        for (int i = 0; i < 25; i ++) {
+            for (Object item : queue) {
+                System.out.print(item + " ");
+            }
+            System.out.println();
+            queue.remove();
+        }
+    }
+
+}
---
+++ src/collections/Collections.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package collections;
+
+import Lists.QueueExample;
+
+/**
+ *
+ * @author Cole
+ */
+public class Collections {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+       
+        QueueExample queue = new QueueExample();
+      
+    }
+    
+}
